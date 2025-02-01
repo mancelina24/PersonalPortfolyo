fffe76f9b4a47f754b77a9a0e45e9f5759c5d192
mancelina24 <feyza.turkay@ogr.iu.edu.tr>
2025-02-02 00:59:55 +0300

    13

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,45 @@ import React, { useContext } from "react";
 import { UserContext } from "../contexts/UserContext.jsx";
 import { ENGLISH, languageEng } from "../api/dataEng.js";
 import { TURKCE, languageTr } from "../api/dataTr.js";
+
 import Switch from "@mui/material/Switch";
 
 const Header = () => {
   const label = { inputProps: { "aria-label": "Size switch demo" } };
+
   const { languageChange, toggleTheme, language, themeName, darkMode } =
     useContext(UserContext);
   return (
-    <div className="flex flex-row justify-around ">
-      <div>
-        <h1 className="text-4xl font-bold">Feyza Türkay</h1>
+    <div className="flex flex-row justify-end items-center bg-none">
+      <div className="flex items-center cursor-pointer">
+        <label>{themeName}</label>
+        <Switch
+          {...label}
+          checked={darkMode}
+          onChange={toggleTheme}
+          sx={{
+            width: 55,
+            height: 24,
+            padding: 0,
+            display: "flex",
+            "& .MuiSwitch-thumb": {
+              backgroundColor: /*darkMode ? "#8F88FF" :*/ "#FFE86E", // Thumb (circle) color change
+              width: 15, // Thumb (circle) width
+              height: 15, // Thumb (circle) height
+              //transform: "translateX(15px)", // Slightly shift the thumb for better alignment
+              transform: "translateY(-7px)", // Slightly shift the thumb for better alignment
+            },
+            // "& .MuiSwitch-switchBase.Mui-checked": {
+            //   transform: "translateY(2px)",
+            // },
+            "& .MuiSwitch-track": {
+              backgroundColor: darkMode ? "red" : "#8F88FF", // Track (main background) color change
+              borderRadius: "9999px", // Keep the track rounded
+              width: 60, // Thumb (circle) width
+              height: 20, // Thumb (circle) height
+            },
+          }}
+        />
       </div>
       <div className="flex flex-row">
         <label onClick={languageChange}>
@@ -19,38 +48,6 @@ const Header = () => {
             ? languageEng.tr + languageEng.ekTr
             : languageTr.ekEng + languageTr.eng}
         </label>
-        <div className="flex items-center space-x-3">
-          <div className="flex items-center cursor-pointer">
-            <Switch
-              {...label}
-              checked={darkMode}
-              onChange={toggleTheme}
-              sx={{
-                width: 55,
-                height: 24,
-                padding: 0,
-                display: "flex",
-                "& .MuiSwitch-thumb": {
-                  backgroundColor: /*darkMode ? "#8F88FF" :*/ "#FFE86E", // Thumb (circle) color change
-                  width: 15, // Thumb (circle) width
-                  height: 15, // Thumb (circle) height
-                  //transform: "translateX(15px)", // Slightly shift the thumb for better alignment
-                  transform: "translateY(-7px)", // Slightly shift the thumb for better alignment
-                },
-                // "& .MuiSwitch-switchBase.Mui-checked": {
-                //   transform: "translateY(2px)",
-                // },
-                "& .MuiSwitch-track": {
-                  backgroundColor: darkMode ? "red" : "#8F88FF", // Track (main background) color change
-                  borderRadius: "9999px", // Keep the track rounded
-                  width: 60, // Thumb (circle) width
-                  height: 20, // Thumb (circle) height
-                },
-              }}
-            />
-            <label>{themeName}</label>
-          </div>
-        </div>
       </div>
     </div>
   );
