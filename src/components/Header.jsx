01cd13deb6b6287974a03102c66f53ef37e9b21b
mancelina24 <feyza.turkay@ogr.iu.edu.tr>
2025-02-01 21:53:33 +0300

    6

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { UserContext } from "../contexts/UserContext.jsx";
 import { ENGLISH, languageEng } from "../api/dataEng.js";
 import { TURKCE, languageTr } from "../api/dataTr.js";
+import { RxSwitch } from "react-icons/rx";
 
 const Header = () => {
   const { languageChange, toggleTheme, language, themeName } =
@@ -12,16 +13,16 @@ const Header = () => {
         <h1 className="text-4xl font-bold">Feyza Türkay</h1>
       </div>
       <div>
-        <label>
-          <input onClick={toggleTheme} />
-
-          {themeName}
-        </label>
-
         <label onClick={languageChange}>
           {language === TURKCE
             ? languageEng.tr + languageEng.ekTr
             : languageTr.ekEng + languageTr.eng}
+        </label>
+        <RxSwitch size={42} />
+        <label>
+          <input onClick={toggleTheme} />
+
+          {themeName}
         </label>
       </div>
     </div>
