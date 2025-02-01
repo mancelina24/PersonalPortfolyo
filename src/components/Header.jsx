89dcf2506351a66fb5acb312d140ef0f4065d45d
mancelina24 <feyza.turkay@ogr.iu.edu.tr>
2025-02-01 22:44:29 +0300

    10

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,11 @@ import React, { useContext } from "react";
 import { UserContext } from "../contexts/UserContext.jsx";
 import { ENGLISH, languageEng } from "../api/dataEng.js";
 import { TURKCE, languageTr } from "../api/dataTr.js";
-import { RxSwitch } from "react-icons/rx";
+import Switch from "@mui/material/Switch";
 
 const Header = () => {
-  const { languageChange, toggleTheme, language, themeName } =
+  const label = { inputProps: { "aria-label": "Size switch demo" } };
+  const { languageChange, toggleTheme, language, themeName, darkMode } =
     useContext(UserContext);
   return (
     <div className="flex flex-row justify-around">
@@ -18,12 +19,31 @@ const Header = () => {
             ? languageEng.tr + languageEng.ekTr
             : languageTr.ekEng + languageTr.eng}
         </label>
-        <RxSwitch size={42} className="cursor-pointer" />
-        <label>
-          <input onClick={toggleTheme} />
-
-          {themeName}
-        </label>
+        <div className="flex items-center space-x-3">
+          <div className="flex items-center cursor-pointer">
+            <Switch
+              {...label}
+              checked={darkMode}
+              onChange={toggleTheme}
+              sx={{
+                "& .MuiSwitch-thumb": {
+                  backgroundColor: darkMode ? "#8F88FF" : "#FFE86E", // Thumb (circle) color change
+                  width: 15, // Thumb (circle) width
+                  height: 15, // Thumb (circle) height
+                  transform: "translateX(9px)", // Slightly shift the thumb for better alignment
+                  transform: "translateY(5px)", // Slightly shift the thumb for better alignment
+                },
+                "& .MuiSwitch-track": {
+                  backgroundColor: darkMode ? "#FFE86E" : "#8F88FF", // Track (main background) color change
+                  borderRadius: "9999px", // Keep the track rounded
+                  width: 50, // Thumb (circle) width
+                  height: 20, // Thumb (circle) height
+                },
+              }}
+            />
+            <label>{themeName}</label>
+          </div>
+        </div>
       </div>
     </div>
   );
